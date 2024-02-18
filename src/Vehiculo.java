3fab40d3435058c773b58f2323b5529b2341daf1
Inumikael <159866200+Inumikael@users.noreply.github.com>
2024-02-18 13:19:08 -0600

    Add files via upload

---
+++ src/Vehiculo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author invitado
+ */
+public class Vehiculo {
+    //atributos (estado) de la clase 
+       /*  
+          marca
+          modelo
+          año
+          color
+          placas
+          valor
+      */
+  
+    private String marca;
+    private String modelo;
+    private int annio;
+    private String color;
+    private double valor;
+
+   
+}
